8ffad9ad05a0cc80ff3131da6b2593d3dd7c1fed
enrique cipriano <76191555+ENRIQUE0957@users.noreply.github.com>
2022-04-01 19:26:36 -0500

    Splitting components for expense Date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,6 +2,7 @@ import '../components/ExpenseItem.css'
 function ExpenseItem(props) {
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+    const year = props.date.getFullYear();
    
     return(
     <div className="expense-item">
@@ -9,6 +10,12 @@ function ExpenseItem(props) {
         <div>{month}</div>
    
         <div>{day}</div>
+        
+        
+       
+        <div class = "expense-item-year">{year}</div>
+        
+        
 
         <div className="expense-item__description">
             <h2 >{props.title}</h2>
